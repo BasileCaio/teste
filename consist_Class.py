8a74071f12fe1281d28e8d061f32aa26bed67b70
Caio basile <fundos.inest@gmail.com>
2023-01-27 11:53:12 -0300

    Adicionei a validação de duplicidade de indxador

--- consist_Class.py
+++ consist_Class.py
@@ -44,51 +44,68 @@ class Consistencia():
         for col in colErrosPessoas:
             rows = list(checkarpess[col][checkarpess[col]==True].index)
             for row in rows:
-                missing_pess.append((self.dfpessoas['Id-SGC'][row], self.dfpessoas['Indexador'][row], self.dfpessoas['ID Pessoa'][row], self.dfpessoas['Pessoa'][row], col, value))
-        self.errospess = pd.DataFrame(data = missing_pess, columns= ['ID-SGC', 'Indexador', 'ID Pessoa', 'Pessoa', 'Rótulo da Questão', 'Erro de resposta encontrado'])
+                missing_pess.append((self.dfpessoas["Id-SGC"][row], self.dfpessoas["Indexador"][row], self.dfpessoas["ID Pessoa"][row], self.dfpessoas["Pessoa"][row], col, value))
+        self.errospess = pd.DataFrame(data = missing_pess, columns= ["ID-SGC", "Indexador", "ID Pessoa", "Pessoa", "Rótulo da Questão", "Erro de resposta encontrado"])
         return self.errospess
+
+
     #Validação de indexadores
-    def valida_indexadores (self, df):
+    def Duplicidade_indexadores_na_Base (self, df):
     #Criei uma lista com o nome das colunas que eu quer dabase inteira, para esta validação
-        colunas_procuradas = ['ID-SGC','Indexador', 'ID-SGS:']
+        colunas_procuradas = ["Id-SGC","Indexador", "ID-SGS:"]
     #pegei o mesmo df que criei e usei nas validações de missing
         df = self.dfpropriedade
         base_de_dados = pd.DataFrame(data = df, columns= colunas_procuradas)
     #Duplicated procura por valores duplicados com o referencial das ([colunas que se passa dentro do colchetes]), 
     #Por padrão preserva o primeiro valor e aponta os outros (keep='first') ou preserva o ultimo e aponta os anteriores (keep='last')
-        self.index_dupli = base_de_dados[base_de_dados.duplicated(['C1'], keep='first')]
+        self.index_dupli = base_de_dados[base_de_dados.duplicated(["Indexador"], keep="first")]
+        self.index_dupli["Erro encontrado"] = "Indexador já existe - Duplicado"
         return self.index_dupli
     #procurando por cpf que ja existem na F1
     #esta função recebe 2 parâmetros, nome do arquivo primero e aba depois. Ambas, strings.
 
-    def cpf_Duplicado(self, arquivo, aba):
+    def indexador_Duplicado_F1xF2(self, arquivo, aba):
+        indexador_F1 = pd.DataFrame(pd.read_excel(f"{arquivo}.xlsx", sheet_name = f"{aba}"))
+        series_indexador_F1 = list( indexador_F1["C1"])
+        verifica_indexadores = self.dfpropriedade[self.dfpessoas["Indexador"].isin(series_indexador_F1)]
+        indexadores_Duplicados = verifica_indexadores[["Id-SGC", "Indexador", "ID-SGS:", "Data da criação"]]
+        indexador_duplicadoF2 = pd.DataFrame(data = indexadores_Duplicados)
+        indexador_duplicadoF2["Erro Encontrado"] = "Indexador encontrado na Fase 1 - Duplicado"
+        return indexador_duplicadoF2
+        
+
+    def cpf_Duplicado_F1xF2(self, arquivo, aba):
         cpf_fase_1 = pd.DataFrame(pd.read_excel(f"{arquivo}.xlsx", sheet_name = f"{aba}"))
         series_cpf_F1 = list(cpf_fase_1["CPF"])
-        erros = self.dfpessoas[self.dfpessoas["Qual é o seu número de CPF?"].isin(series_cpf_F1)]
-        data_erros = erros[["Id-SGC","ID Pessoa", "Pessoa", "Qual é o seu número de CPF?", "Possui ID-SGS-Pessoa? - Sim - ID-SGS-Pessoa:"]]
-        cpf_duplicadoF2 = pd.DataFrame(data = data_erros)
+        verifica_CPF = self.dfpessoas[self.dfpessoas["Qual é o seu número de CPF?"].isin(series_cpf_F1)]
+        cpf_Duplicados = verifica_CPF[["Id-SGC","ID Pessoa", "Pessoa", "Qual é o seu número de CPF?", "Possui ID-SGS-Pessoa? - Sim - ID-SGS-Pessoa:"]]
+        cpf_duplicadoF2 = pd.DataFrame(data = cpf_Duplicados)
+        cpf_duplicadoF2["Erro Encontrado"] = "CPF encontrado na Fase 1 - Duplicado"
         return cpf_duplicadoF2
 
-    def sgs_Duplicado(self, arquivo):
-        sgs_fase_1 = pd.DataFrame(pd.read_excel(f"{arquivo}.xlsx", sheet_name = "prop"))
-        series_sgs_F1 = list(sgs_fase_1["ID SGS pessoa"])
-        series_sgs_F2 = self.dfpropriedade[self.dfpropriedade["ID-SGS:"].isin(series_sgs_F1)]
-        data_erros = series_sgs_F2[["Id-SGC","ID Pessoa", "Pessoa", "Qual é o seu número de CPF?", "Possui ID-SGS-Pessoa? - Sim - ID-SGS-Pessoa:"]]
-        cpf_duplicadoF2 = pd.DataFrame(data = data_erros)
-        return cpf_duplicadoF2
-#self.dfpropriedade[self.dfpropriedade["ID-SGS:"].isin(series_sgs_F1)]
+    def idsgc_DuplicadoF1xF2(self, arquivo, aba):
+            idsgc_fase_1 = pd.DataFrame(pd.read_excel(f"{arquivo}.xlsx", sheet_name = f"{aba}"))
+            series_idsgc_F1 = list(idsgc_fase_1["ID-SGC"])
+            verifica_IDSGC = self.dfpropriedade[self.dfpessoas["Id-SGC"].isin(series_idsgc_F1)]
+            idsgc_duplicados = verifica_IDSGC[["Id-SGC", "Indexador", "ID-SGS:", "Data da criação"]]
+            df_IDGSC_duplicadoF2 = pd.DataFrame(data = idsgc_duplicados)
+            df_IDGSC_duplicadoF2["Erro encontrado"] = "ID-SGC consta na Fase 1 - Duplicado"
+            return df_IDGSC_duplicadoF2
+
+
    
 
 
 objeto = Consistencia()
 #lerbase de pessoas e propridades
-
 objeto.abrir_bases("BD_148.00")
-
 #procurar os missings em propriedades 
 objeto.valida_missing_propriedades(objeto.dfpropriedade, '[missing]')
 #procurarmissings em pessoas
-
 objeto.valida_missing_pessoas(objeto.dfpessoas, '[missing]')
-objeto.valida_indexadores(objeto.dfpropriedade)
-objeto.cpf_Duplicado("controle_pessoas", "Estrutura")
+#procurar indexadores duplicados na base
+objeto.Duplicidade_indexadores_na_Base(objeto.dfpropriedade)
+#validar se os cpf's da Fase 1 são diferentes dos cpf's da Fase 2 e apontar caso sejam iguais 
+objeto.cpf_Duplicado_F1xF2("controle_pessoas", "Estrutura")
+objeto.idsgc_DuplicadoF1xF2("SGS_controle_fases", "Estrutura")
+objeto.indexador_Duplicado_F1xF2("SGS_controle_fases", "Estrutura")
